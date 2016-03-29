8c3feca2a12af5cf518252cd4fa1361dadfb4d4b
Nick Doiron <nicholas.doiron@asiafoundation.org>
2016-03-29 14:55:55 -0700

    Neo4j py2neo support

--- scrape.py
+++ scrape.py
@@ -1,5 +1,12 @@
 from lxml import html
 import requests
+from py2neo import Graph, Node, Relationship
+
+g = Graph(user="neo4j", password="admin")
+tx = g.begin()
+a = Node("Person", name="Alice")
+tx.create(a)
+tx.commit()
 
 page = requests.get('http://moma.org/collection/works/2')
 tree = html.fromstring(page.content)
