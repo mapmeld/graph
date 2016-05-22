5bbcfff6e3c5da835d1c933ea94b9ef42eafb7f5
Nick Doiron <ndoiron@mapmeld.com>
2016-05-22 16:28:14 +0700

    create relationship with py2neo

--- scrape.py
+++ scrape.py
@@ -2,10 +2,18 @@ from lxml import html
 import requests
 from py2neo import Graph, Node, Relationship
 
-g = Graph(user="neo4j", password="admin")
+g = Graph(user="neo4j", password="Swift")
 tx = g.begin()
+
 a = Node("Person", name="Alice")
 tx.create(a)
+
+b = Node("Person", name="Bob")
+tx.create(b)
+
+c = Relationship(a, "KNOWS", b, order=1)
+tx.create(c)
+
 tx.commit()
 
 page = requests.get('http://moma.org/collection/works/2')
