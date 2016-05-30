d57ab6248c1e1820074c449499e52712b4fab0d3
Nick Doiron <ndoiron@mapmeld.com>
2016-05-30 14:37:43 +0700

    sample info

--- scrape.py
+++ scrape.py
@@ -3,20 +3,13 @@ import requests
 from py2neo import Graph, Node, Relationship
 
 g = Graph(user="neo4j", password="Swift")
+
 tx = g.begin()
-
-a = Node("Person", name="Alice")
-tx.create(a)
-
-b = Node("Person", name="Bob")
-tx.create(b)
-
-c = Relationship(a, "KNOWS", b, order=1)
-tx.create(c)
-
-tx.commit()
 
 page = requests.get('http://moma.org/collection/works/2')
 tree = html.fromstring(page.content)
 
-print(len(tree.cssselect('div')))
+tree.cssselect('div')
+
+
+tx.commit()
